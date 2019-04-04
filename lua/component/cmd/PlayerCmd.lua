7a064e60fd7caef3d353ee03993c0594c82b79c6
ouchzsc <ouchzsc@gmail.com>
2019-04-04 11:46:34 +0800

    delete BulletAttack.lua

--- lua/component/cmd/PlayerCmd.lua
+++ lua/component/cmd/PlayerCmd.lua
@@ -1,12 +1,9 @@
 local PlayerCmd = Component:extends()
-local defaulAttackCd = 0.2
+
 function PlayerCmd:onEnable()
     local entity = self.entity
     self:reg(event.onCmdUpdate, function(dt)
         entity.cmdX, entity.cmdY = utils.getAxisWasd()
-        if love.mouse.isDown(1) then
-            self:onAttackPressing()
-        end
         local x, y = love.mouse.getPosition()
         local camScale = camMgr.get().scale
         if camScale == nil then
@@ -17,20 +14,6 @@ function PlayerCmd:onEnable()
         if wx < entity.x then
             entity.dir = -1
         end
-    end)
-end
-
-function PlayerCmd:onAttackPressing()
-    local entity = self.entity
-    if entity.isAttacking then
-        return
-    end
-    local entity = self.entity
-    entity.isAttacking = true
-    entity:popEvent("attack")
-    self:scheduleTimer("attacking", self.attackCd or defaulAttackCd, function()
-        entity.isAttacking = false
-        entity:popEvent("stopAttacking")
     end)
 end
 
